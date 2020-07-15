54d1a6551d67e575ef35b902c0222210d923c43e
Joao Sousa <joao.rsousa23@gmail.com>
2020-07-15 20:41:50 +0930

    added appoint models, routes, repositories e services

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,13 @@
+import 'reflect-metadata';
 import express from 'express';
 
-import routes from './routes';
+import appointmentsRouter from './routes/appointments.routes';
 import './database';
 
 const app = express();
 
 app.use(express.json());
-app.use(routes);
+app.use(appointmentsRouter);
 
 app.listen(3333, () => {
   console.log('server stated on port 3333!');
